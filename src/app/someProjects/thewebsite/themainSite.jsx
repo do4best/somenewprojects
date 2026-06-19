4e3c134af7a29a0a6342186176d017589f1110f5
do4best657 <panic_4_what@hotmail.com>
2026-06-19 17:42:39 +0500

    dark mode is handled with help of junei Amazing work

---
+++ src/app/someProjects/thewebsite/themainSite.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Typography} from "@mui/material";
+
+function ThemainSite(props) {
+    return (
+        <>
+        <Typography variant="h1">The Website</Typography>
+
+        </>
+    );
+}
+
+export default ThemainSite;
